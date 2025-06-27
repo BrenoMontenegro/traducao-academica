16c4c68c0cd5ca99221afebd49e203880aab0b64
BrenoMontenegro <breno.freitas1608@gmail.com>
2025-06-27 02:18:31 +0000

    mais alteracoes visuais nas paginas principais

--- backend/meu_app/static/meu_app/script.js
+++ backend/meu_app/static/meu_app/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-            //Aqui é a parte da animação da caixa de deletar resumo
+            
             const modal = document.getElementById('modalConfirmacao');
             const form = document.getElementById('form-deletar-modal');
             const tituloSpan = document.getElementById('titulo-resumo');
@@ -11,23 +11,5 @@ document.addEventListener('DOMContentLoaded', function () {
         
                 form.action = `/deletar-resumo/${resumoId}/`;
                 tituloSpan.textContent = `"${tituloResumo}"`;
-            });
-            
-            
-            //Essa parte fica reservada para o código do darkmode
-            const toggleButton = document.getElementById('toggle-darkmode');
-            const body = document.body;
-            
-                
-            if (localStorage.getItem('modo') === 'dark') {
-                body.classList.add('dark-mode');
-                toggleButton.textContent = '☀️';
-            }
-            
-            toggleButton.addEventListener('click', () => {
-              body.classList.toggle('dark-mode');
-              const escureceu = body.classList.contains('dark-mode');
-              toggleButton.textContent = escureceu ? '☀️' : '🌙';
-              localStorage.setItem('modo', escureceu ? 'dark' : 'light');
             });
         });
